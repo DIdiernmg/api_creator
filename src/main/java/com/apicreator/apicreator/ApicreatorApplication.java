c09454322496f0625f9757b07c8a7ba39f33a740
DIdiernmg <insidier@gmail.com>
2025-06-16 15:27:36 -0500

    primer commit

---
+++ src/main/java/com/apicreator/apicreator/ApicreatorApplication.java
@@ -0,0 +1,13 @@
+package com.apicreator.apicreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicreator/apicreator/ApicreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicreator.apicreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
